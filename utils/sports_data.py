96f37b951a4afa110ca8be0ee7eee1ebf719f8e8
MonkeyMuscle121 <cryptoleveller@yahoo.com>
2026-06-06 22:28:16 +0100

    Delete utils directory

--- utils/sports_data.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import pytz
-
-async def fetch_upcoming_fixtures(sport: str, hours: int = 48):
-    now = datetime.now(pytz.utc)
-    return {
-        "sport": sport,
-        "current_time": now.strftime("%Y-%m-%d %H:%M UTC")
-    }
